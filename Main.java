30818a0052160ac0bdbb4efcc9ca5b9b18f7aeb3
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2024-11-13 19:05:07 +0000

    skibidi toilet

--- Main.java
+++ Main.java
@@ -4,6 +4,31 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    String sentence = "Peter Piper Picked a Pack of Pickle Peppers";
+    int count = 0;
+    for (int i = 0; i < sentence.length()-1; i++)
+    {
+      String pair = sentence.substring(i, i+2);
+      if (pair.equals("pa") || pair.equals("pe") || pair.equals("po") || pair.equals("pi"))
+      {
+        count++;
+      }
+      System.out.println("p followed by a vowel appears " + count + "times");
+    }
+    String sentence = "gagagigo the giga risen";
+    String target = "ga";
+    int count = 0;
+
+    for (int i = 0; i < sentence.length()-1; i++)
+    {
+      String length = sentence.substring(i, i+2);
+      if (length.equals(target))
+      {
+        count++; 
+      }
+      return count;
+    }
+    Scanner scanner = new scanner(System.in)
+    System.out.print("Enter an inte")  
   }
 }
